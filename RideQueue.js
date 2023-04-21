ed1e0d68b1615f59ae0bd7349e411d69bc2067b6
Shivendra Singh <shivendrapaliwal99990@gmail.com>
2023-04-21 15:05:28 +0530

    Ride Queue

---
+++ RideQueue.js
@@ -0,0 +1,46 @@
+
+function runProgram(input) {
+    // Write code here
+    input = input.split("\n")
+    let tc = +input[0]
+    let line = 1
+    for(let i=0;i<tc;i++){
+        let n = +input[line++]
+        let arr = input[line++].split(" ").map(Number)
+        RideQueue(n,arr)
+    }
+  }
+  
+  if (process.env.USER === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
+  function RideQueue(n,arr){
+      let count = 0
+      for(let i=0;i<n;i++){
+          if(arr[i]<arr[i+1]){
+              continue;
+          }else{
+              count++
+          }
+      }
+      console.log(count)
+  }
+  
