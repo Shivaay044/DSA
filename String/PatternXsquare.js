b4f179f38c2a6d0ef0f65ad9f01241052d6c951a
Shivendra Singh <shivendrapaliwal99990@gmail.com>
2023-05-17 21:30:54 +0530

    X square

---
+++ String/PatternXsquare.js
@@ -0,0 +1,15 @@
+function patternPrinting(N) {
+    // Write code here
+    for(let i=0;i<N;i++){
+        let bag=""
+        for(let j=0;j<N;j++){
+            if(i==0||i==N-1||j==0||j==N-1||i-j==0||i+j==N-1){
+                bag+='*'+" "
+            }
+            else{
+                bag+="  "
+            }
+        }
+        console.log(bag)
+    }
+}
