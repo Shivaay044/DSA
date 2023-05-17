c83ffeeb12f98568d264b4a1086de9f23bd2c623
Shivendra Singh <shivendrapaliwal99990@gmail.com>
2023-05-17 23:08:45 +0530

    forum parking mall

---
+++ Arrays/forumMallParking.js
@@ -0,0 +1,14 @@
+function  forumMallparking(K,arv,dep){
+	// Write code here
+	let c = 1
+	for(let i=0;i<arv.length-1;i++){
+	    if(dep[i]>arv[i+1]){
+	        c++
+	    }
+	}
+	if(c<=K){
+	    console.log("Possible")
+	}else{
+	    console.log("Not Possible")
+	}
+}
